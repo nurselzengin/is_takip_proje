8ec528d5eeb4b3bd2526bba69fcb2affb01e3d46
nurselzengin <125567986+nurselzengin@users.noreply.github.com>
2023-11-20 15:09:15 +0300

    Buton metotları eklendi

--- is_takip_proje/Formlar/FrmDepartmanlar.Designer.cs
+++ is_takip_proje/Formlar/FrmDepartmanlar.Designer.cs
@@ -30,137 +30,152 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDepartmanlar));
             this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
-            this.simpleButton4 = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
-            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
-            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.BtnGüncelle = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.TxtAd = new DevExpress.XtraEditors.TextEdit();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.BtnListele = new DevExpress.XtraEditors.SimpleButton();
+            this.TxtID = new DevExpress.XtraEditors.TextEdit();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.gridControl1 = new DevExpress.XtraGrid.GridControl();
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
             this.groupControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtAd.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtID.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupControl1
             // 
-            this.groupControl1.Controls.Add(this.simpleButton4);
-            this.groupControl1.Controls.Add(this.simpleButton3);
-            this.groupControl1.Controls.Add(this.simpleButton2);
-            this.groupControl1.Controls.Add(this.textEdit2);
+            this.groupControl1.Controls.Add(this.BtnGüncelle);
+            this.groupControl1.Controls.Add(this.BtnSil);
+            this.groupControl1.Controls.Add(this.BtnEkle);
+            this.groupControl1.Controls.Add(this.TxtAd);
             this.groupControl1.Controls.Add(this.labelControl2);
-            this.groupControl1.Controls.Add(this.simpleButton1);
-            this.groupControl1.Controls.Add(this.textEdit1);
+            this.groupControl1.Controls.Add(this.BtnListele);
+            this.groupControl1.Controls.Add(this.TxtID);
             this.groupControl1.Controls.Add(this.labelControl1);
-            this.groupControl1.Location = new System.Drawing.Point(628, 0);
+            this.groupControl1.Location = new System.Drawing.Point(471, 0);
+            this.groupControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.groupControl1.Name = "groupControl1";
-            this.groupControl1.Size = new System.Drawing.Size(338, 320);
+            this.groupControl1.Size = new System.Drawing.Size(254, 260);
             this.groupControl1.TabIndex = 5;
-            this.groupControl1.Text = "groupControl1";
             // 
-            // simpleButton4
+            // BtnGüncelle
             // 
-            this.simpleButton4.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton4.ImageOptions.Image")));
-            this.simpleButton4.Location = new System.Drawing.Point(129, 246);
-            this.simpleButton4.Name = "simpleButton4";
-            this.simpleButton4.Size = new System.Drawing.Size(161, 41);
-            this.simpleButton4.TabIndex = 7;
-            this.simpleButton4.Text = "Güncelle";
+            this.BtnGüncelle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnGüncelle.ImageOptions.Image")));
+            this.BtnGüncelle.Location = new System.Drawing.Point(97, 200);
+            this.BtnGüncelle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnGüncelle.Name = "BtnGüncelle";
+            this.BtnGüncelle.Size = new System.Drawing.Size(121, 33);
+            this.BtnGüncelle.TabIndex = 7;
+            this.BtnGüncelle.Text = "Güncelle";
             // 
-            // simpleButton3
+            // BtnSil
             // 
-            this.simpleButton3.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
-            this.simpleButton3.Location = new System.Drawing.Point(129, 199);
-            this.simpleButton3.Name = "simpleButton3";
-            this.simpleButton3.Size = new System.Drawing.Size(161, 41);
-            this.simpleButton3.TabIndex = 6;
-            this.simpleButton3.Text = "Sil";
+            this.BtnSil.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnSil.ImageOptions.Image")));
+            this.BtnSil.Location = new System.Drawing.Point(97, 162);
+            this.BtnSil.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(121, 33);
+            this.BtnSil.TabIndex = 6;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
             // 
-            // simpleButton2
+            // BtnEkle
             // 
-            this.simpleButton2.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
-            this.simpleButton2.Location = new System.Drawing.Point(129, 152);
-            this.simpleButton2.Name = "simpleButton2";
-            this.simpleButton2.Size = new System.Drawing.Size(161, 41);
-            this.simpleButton2.TabIndex = 5;
-            this.simpleButton2.Text = "Ekle";
+            this.BtnEkle.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnEkle.ImageOptions.Image")));
+            this.BtnEkle.Location = new System.Drawing.Point(97, 124);
+            this.BtnEkle.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnEkle.Name = "BtnEkle";
+            this.BtnEkle.Size = new System.Drawing.Size(121, 33);
+            this.BtnEkle.TabIndex = 5;
+            this.BtnEkle.Text = "Ekle";
+            this.BtnEkle.Click += new System.EventHandler(this.BtnEkle_Click);
             // 
-            // textEdit2
+            // TxtAd
             // 
-            this.textEdit2.Location = new System.Drawing.Point(102, 77);
-            this.textEdit2.Name = "textEdit2";
-            this.textEdit2.Size = new System.Drawing.Size(225, 22);
-            this.textEdit2.TabIndex = 4;
+            this.TxtAd.Location = new System.Drawing.Point(76, 63);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(169, 20);
+            this.TxtAd.TabIndex = 4;
             // 
             // labelControl2
             // 
-            this.labelControl2.Location = new System.Drawing.Point(6, 80);
+            this.labelControl2.Location = new System.Drawing.Point(4, 65);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.labelControl2.Name = "labelControl2";
-            this.labelControl2.Size = new System.Drawing.Size(90, 16);
+            this.labelControl2.Size = new System.Drawing.Size(75, 13);
             this.labelControl2.TabIndex = 3;
             this.labelControl2.Text = "Departman Adı:";
             // 
-            // simpleButton1
+            // BtnListele
             // 
-            this.simpleButton1.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
-            this.simpleButton1.Location = new System.Drawing.Point(129, 105);
-            this.simpleButton1.Name = "simpleButton1";
-            this.simpleButton1.Size = new System.Drawing.Size(161, 41);
-            this.simpleButton1.TabIndex = 2;
-            this.simpleButton1.Text = "Listele";
-            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            this.BtnListele.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnListele.ImageOptions.Image")));
+            this.BtnListele.Location = new System.Drawing.Point(97, 85);
+            this.BtnListele.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.BtnListele.Name = "BtnListele";
+            this.BtnListele.Size = new System.Drawing.Size(121, 33);
+            this.BtnListele.TabIndex = 2;
+            this.BtnListele.Text = "Listele";
+            this.BtnListele.Click += new System.EventHandler(this.simpleButton1_Click);
             // 
-            // textEdit1
+            // TxtID
             // 
-            this.textEdit1.Location = new System.Drawing.Point(102, 38);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Size = new System.Drawing.Size(225, 22);
-            this.textEdit1.TabIndex = 1;
+            this.TxtID.Location = new System.Drawing.Point(76, 31);
+            this.TxtID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TxtID.Name = "TxtID";
+            this.TxtID.Size = new System.Drawing.Size(169, 20);
+            this.TxtID.TabIndex = 1;
             // 
             // labelControl1
             // 
-            this.labelControl1.Location = new System.Drawing.Point(6, 41);
+            this.labelControl1.Location = new System.Drawing.Point(4, 33);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.labelControl1.Name = "labelControl1";
-            this.labelControl1.Size = new System.Drawing.Size(84, 16);
+            this.labelControl1.Size = new System.Drawing.Size(71, 13);
             this.labelControl1.TabIndex = 0;
             this.labelControl1.Text = "Departman ID:";
             // 
             // gridControl1
             // 
-            this.gridControl1.Location = new System.Drawing.Point(-4, 0);
+            this.gridControl1.EmbeddedNavigator.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gridControl1.Location = new System.Drawing.Point(-3, 0);
             this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(632, 320);
+            this.gridControl1.Size = new System.Drawing.Size(474, 260);
             this.gridControl1.TabIndex = 6;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
             // 
             // gridView1
             // 
+            this.gridView1.DetailHeight = 284;
             this.gridView1.GridControl = this.gridControl1;
             this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsEditForm.PopupEditFormWidth = 600;
             // 
             // FrmDepartmanlar
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(967, 318);
+            this.ClientSize = new System.Drawing.Size(725, 258);
             this.Controls.Add(this.gridControl1);
             this.Controls.Add(this.groupControl1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FrmDepartmanlar";
             this.Text = "Departmanlar";
             this.Load += new System.EventHandler(this.FrmDepartmanlar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
             this.groupControl1.ResumeLayout(false);
             this.groupControl1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtAd.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TxtID.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             this.ResumeLayout(false);
@@ -169,14 +184,14 @@
 
         #endregion
         private DevExpress.XtraEditors.GroupControl groupControl1;
-        private DevExpress.XtraEditors.SimpleButton simpleButton1;
-        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton BtnListele;
+        private DevExpress.XtraEditors.TextEdit TxtID;
         private DevExpress.XtraEditors.LabelControl labelControl1;
-        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit TxtAd;
         private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraEditors.SimpleButton simpleButton2;
-        private DevExpress.XtraEditors.SimpleButton simpleButton4;
-        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraEditors.SimpleButton BtnEkle;
+        private DevExpress.XtraEditors.SimpleButton BtnGüncelle;
+        private DevExpress.XtraEditors.SimpleButton BtnSil;
         private DevExpress.XtraGrid.GridControl gridControl1;
         private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
     }
--- is_takip_proje/Formlar/FrmDepartmanlar.cs
+++ is_takip_proje/Formlar/FrmDepartmanlar.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using is_takip_proje.Entity;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraEditors;
 
 namespace is_takip_proje.Formlar
 {
@@ -17,14 +19,48 @@ namespace is_takip_proje.Formlar
             InitializeComponent();
         }
 
+        Dbis_TakipEntities1 db = new Dbis_TakipEntities1();
+
+        void Listele() 
+        {
+            var degerler = (from x in db.TblDepartmanlar
+                           select new 
+                           { 
+                                x.ID,
+                                x.Ad
+                           }).ToList();
+            gridControl1.DataSource = degerler;
+        }
+
         private void FrmDepartmanlar_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void simpleButton1_Click(object sender, EventArgs e)
         {
+            Listele();
+        }
 
+        private void BtnEkle_Click(object sender, EventArgs e)
+        {
+            TblDepartmanlar t = new TblDepartmanlar();
+            t.Ad = TxtAd.Text;
+            db.TblDepartmanlar.Add(t);
+            db.SaveChanges();
+            XtraMessageBox.Show("Departman başarılı bir şekilde sisteme kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Listele();
+
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(TxtID.Text);
+            var deger = db.TblDepartmanlar.Find(x);
+            db.TblDepartmanlar.Remove(deger);
+            db.SaveChanges();
+            XtraMessageBox.Show("Departman silme işlemi başarılı bir şekilde gerçekleştirildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Listele();
         }
     }
 }
